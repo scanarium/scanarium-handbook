b2b356ccd3f07b47de37c8b324399ed252c4beca
Christian Aistleitner <christian@quelltextlich.at>
2021-05-10 16:47:41 +0200

    Bring in logos for linkbar

--- update-static-content.py
+++ update-static-content.py
@@ -17,6 +17,9 @@ def copy_image(source, target_name=None):
     shutil.copy2(source, target)
 
 
+def process_scanarium_dir(dir):
+    pass
+
 def process_scanarium_support_dir(dir):
     name = os.path.join(
         dir, 'avatar',
@@ -24,13 +27,31 @@ def process_scanarium_support_dir(dir):
     copy_image(name, 'logo-big.png')
 
 
+def process_scanarium_homepage_dir(dir):
+    for image in [
+        'facebook',
+        'instagram',
+        'scanarium',
+        'twitter',
+        'youtube',
+            ]:
+        copy_image(os.path.join(dir, f'dynamisch/images/{image}.png'))
+
+
 def parse_arguments():
     parser = argparse.ArgumentParser(
         description='Updates static content sourced from other repos',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument(
+        '--scanarium-dir', default=None,
+        help='The directory where the `scanarium` repo can be found')
+    parser.add_argument(
+        '--scanarium-homepage-dir', default=None,
+        help='The directory where the `Homepages/scanarium.com` repo can '
+        'be found')
+    parser.add_argument(
         '--scanarium-support-dir', default=None,
-        help='The directory where `scanarium-support` can be found')
+        help='The directory where the `scanarium-support` repo can be found')
 
     return parser.parse_args()
 
@@ -39,12 +60,24 @@ if __name__ == '__main__':
     args = parse_arguments()
     exit_code = 0
 
-    scanarium_support_dir = args.scanarium_support_dir
-    if args.scanarium_support_dir:
-        process_scanarium_support_dir(args.scanarium_support_dir)
-        print('scanarium-support done.')
-    else:
-        exit_code = 1
-        print('No scanarium-support dir given. Skipping corresponding updates')
+    for repo_conf in [
+        {'name': 'scanarium',
+         'directory': args.scanarium_dir,
+         'processor': process_scanarium_dir},
+        {'name': 'scanarium-support',
+         'directory': args.scanarium_support_dir,
+         'processor': process_scanarium_support_dir},
+        {'name': 'scanarium-homepage',
+         'directory': args.scanarium_homepage_dir,
+         'processor': process_scanarium_homepage_dir},
+            ]:
+        dir = repo_conf['directory']
+        if dir:
+            repo_conf['processor'](dir)
+            print(f'`{repo_conf["name"]}` repo done.')
+        else:
+            exit_code = 1
+            print(f'No {repo_conf["name"]} dir given. Skipping corresponding '
+                  'updates')
 
     sys.exit(exit_code)
