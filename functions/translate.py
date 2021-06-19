e04603fc9f40125a8179fffb9ba82a2c2051f620
Christian Aistleitner <christian@quelltextlich.at>
2021-06-19 16:25:07 +0200

    Add translated Language labels

--- functions/translate.py
+++ functions/translate.py
@@ -9,13 +9,16 @@ del sys.path[0]
 LOCALIZER_FACTORY = None
 
 
-def translate(file, state, args):
+def get_localizer(language):
     global LOCALIZER_FACTORY
     if LOCALIZER_FACTORY is None:
         localization_dir = os.path.join(
             FILE_DIR_ABS, 'translate', 'localizations')
         LOCALIZER_FACTORY = LocalizerFactory(localization_dir)
-    localizer = LOCALIZER_FACTORY.get_localizer(file['properties']['language'])
+    return LOCALIZER_FACTORY.get_localizer(language)
+
+def translate(file, state, args):
+    localizer = get_localizer(file['properties']['language'])
     ret = None
     len_args = len(args)
     if len_args == 1:
@@ -27,3 +30,13 @@ def translate(file, state, args):
             f'Unexpected length length {len_args} of argument list')
 
     return ret
+
+def dumpLanguageMatrix(file, state, args):
+    l10ns = file['properties']['build_localizations'].split(',')
+    lst = [];
+    for ui_l10n in l10ns:
+        localizer = get_localizer(ui_l10n)
+        for target_l10n in l10ns:
+            value = localizer.localize_parameter('language', target_l10n)
+            lst.append(f'{ui_l10n}-{target_l10n}:{value}')
+    return ','.join(lst)
